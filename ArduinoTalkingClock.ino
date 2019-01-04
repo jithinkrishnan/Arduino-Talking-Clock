c9483e6d4a2d49c79d7ae25406b353e155fa1386
unknown <jithinkrishnan.k@gmail.com>
2019-01-04 19:21:43 +0530

    added ringing feature

--- ArduinoTalkingClock.ino
+++ ArduinoTalkingClock.ino
@@ -3,7 +3,7 @@
 *   
 *   File:   ArduinoTalkingClock.ino
 *   Author:  Jithin Krishnan.K
-*       Rev. 0.0.1 : 30/12/2018 :  08:47 PM
+*       Rev. 0.0.2 : 04/01/2019 :  07:13 PM
 * 
 * This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
@@ -28,88 +28,122 @@
 #include "SPI.h"
 
 #define SD_ChipSelectPin 10
+#define buttonPin 7
+#define AmpON 4				   	   
 
 DS3231 clock;
 TMRpcm tmrpcm;
 RTCDateTime dt;
+char i, hr;		   
 
 void setup()
 {
     tmrpcm.speakerPin=9;
+	  pinMode(buttonPin, INPUT);						  
+	  pinMode(AmpON, OUTPUT);					   
     Serial.begin(115200);
     clock.begin();
 
+	  // Manual (YYYY, MM, DD, HH, II, SS								   
+	  //clock.setDateTime(2019, 1, 3, 8, 54, 54);										   
   if(!SD.begin(SD_ChipSelectPin)) {
       Serial.println("SD fail");
       return;
     }
     tmrpcm.setVolume(5);
+	tmrpcm.quality(1); 				   
 }
 
 
 void loop()
   {
    dt = clock.getDateTime();
-   Serial.print(dt.year);   Serial.print("-");
-   Serial.print(dt.month);  Serial.print("-");
-   Serial.print(dt.day);    Serial.print(" ");
-   Serial.print(dt.hour);   Serial.print(":");
-   Serial.print(dt.minute); Serial.print(":");
-   Serial.print(dt.second); Serial.println("");
-   TexttoSpeech(dt.hour);
-   delay(500);
-   tmrpcm.play("h.wav");
-   delay(1000);
-   TexttoSpeech(dt.minute);
-   delay(500);
-   tmrpcm.play("m.wav");
-   delay(1000);
-   TexttoSpeech(dt.second);
-   delay(500);
-   tmrpcm.play("s.wav");
-   delay(1000);
-   DayOfWeek(dt.dayOfWeek);
-   delay(500);
-   TexttoSpeech(dt.day);
-   delay(500);
-   SpeechMonth(dt.month);
-   delay(500);
-   tmrpcm.play("2000.wav");
-   delay(1000);
-   TexttoSpeech(dt.year%2000);
-   delay(10000);
+	  
+	 if(dt.hour > 12)
+      hr = dt.hour - 12;
+    else
+      hr = dt.hour;
+      
+   if (!dt.minute && !dt.second) {
+        AmpPowerUp();
+        tmrpcm.play("ring.wav");
+        while(tmrpcm.isPlaying());
+       
+        for(i = 0; i < hr; i++)
+          {
+            tmrpcm.play("ding.wav");
+            while(tmrpcm.isPlaying());
+          }
+          AmpPowerDown();
+      }
+
+      if (dt.minute == 30 && !dt.second) {
+          AmpPowerUp();
+          tmrpcm.play("ring.wav");
+          while(tmrpcm.isPlaying());     
+          tmrpcm.play("ding.wav");
+          while(tmrpcm.isPlaying());
+          AmpPowerDown(); 
+        }
+		
+	if (!digitalRead(buttonPin)) {
+      AmpPowerUp(); 
+      TexttoSpeech(dt.hour);
+      while(tmrpcm.isPlaying());
+      tmrpcm.play("h.wav");
+      while(tmrpcm.isPlaying());
+      TexttoSpeech(dt.minute);
+      while(tmrpcm.isPlaying());
+      tmrpcm.play("m.wav");
+      while(tmrpcm.isPlaying());
+      TexttoSpeech(dt.second);
+      while(tmrpcm.isPlaying());
+      tmrpcm.play("s.wav");
+      while(tmrpcm.isPlaying());
+      DayOfWeek(dt.dayOfWeek);
+      while(tmrpcm.isPlaying());
+      TexttoSpeech(dt.day);
+      while(tmrpcm.isPlaying());
+      SpeechMonth(dt.month);
+      while(tmrpcm.isPlaying());
+      tmrpcm.play("2K.wav");
+      while(tmrpcm.isPlaying());
+      TexttoSpeech(dt.year%2000);
+      while(tmrpcm.isPlaying());
+      AmpPowerDown();
+   }		
 }
 
 void DayOfWeek(char d) 
 {
   switch(d) {
     case 1:
-      tmrpcm.play("monday.wav");
-      delay(500);
+      tmrpcm.play("mon.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 2:
-      tmrpcm.play("tuesday.wav");
-      delay(500);
+      tmrpcm.play("tue.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 3:
-      tmrpcm.play("wednesday.wav");
-      delay(500);
+      tmrpcm.play("wed.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 4:
-      tmrpcm.play("thursday.wav");
-      delay(500);
+      tmrpcm.play("thu.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 5:
-      tmrpcm.play("friday.wav");
-      delay(500);
+      tmrpcm.play("fri.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 6:
-      tmrpcm.play("saturday.wav");
-      delay(500);
+      tmrpcm.play("sat.wav");
+      while(tmrpcm.isPlaying());
       break;
     case 7:
-      tmrpcm.play("sunday.wav");
-      delay(500);
+      tmrpcm.play("sun.wav");
+      while(tmrpcm.isPlaying());
       break;
   }
 }
@@ -118,51 +152,51 @@ void SpeechMonth(char digit)
   switch(digit) {
     case 1:
       tmrpcm.play("jan.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 2:
       tmrpcm.play("feb.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 3:
       tmrpcm.play("mar.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 4:
       tmrpcm.play("apr.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 5:
       tmrpcm.play("may.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 6:
       tmrpcm.play("jun.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 7:
       tmrpcm.play("jul.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 8:
       tmrpcm.play("aug.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 9:
       tmrpcm.play("sep.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 10:
       tmrpcm.play("oct.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 11:
       tmrpcm.play("nov.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 12:
       tmrpcm.play("dec.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;         
   }
 }
@@ -171,315 +205,328 @@ void TexttoSpeech(char digit)
   switch(digit) {
     case 0:
       tmrpcm.play("0.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 1:
       tmrpcm.play("1.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 2:
       tmrpcm.play("2.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 3:
       tmrpcm.play("3.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 4:
       tmrpcm.play("4.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 5:
       tmrpcm.play("5.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 6:
       tmrpcm.play("6.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 7:
       tmrpcm.play("7.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break; 
     case 8:
       tmrpcm.play("8.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 9:
       tmrpcm.play("9.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 10:
       tmrpcm.play("10.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 11:
       tmrpcm.play("11.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 12:
       tmrpcm.play("12.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 13:
       tmrpcm.play("13.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 14:
       tmrpcm.play("14.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 15:
       tmrpcm.play("15.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 16:
       tmrpcm.play("16.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 17:
       tmrpcm.play("17.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 18:
       tmrpcm.play("18.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 19:
       tmrpcm.play("19.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 20:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 21:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("1.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 22:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("2.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 23:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("3.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 24:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("4.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
      case 25:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("5.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
      case 26:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("6.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
      case 27:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("7.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
      case 28:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("8.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 29:
       tmrpcm.play("20.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("9.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 30:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 31:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("1.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 32:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("2.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 33:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("3.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break; 
     case 34:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("4.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 35:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("5.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 36:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("6.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 37:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("7.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 38:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("8.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 39:
       tmrpcm.play("30.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("9.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 40:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 41:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("1.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 42:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("2.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 43:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("3.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break; 
     case 44:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("4.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 45:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("5.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 46:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("6.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;      
     case 47:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("7.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 48:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("8.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 49:
       tmrpcm.play("40.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("9.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 50:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 51:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("1.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 52:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("2.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;  
    case 53:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("3.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 54:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("4.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;  
     case 55:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("5.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 56:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("6.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;  
     case 57:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("7.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 58:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("8.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;
     case 59:
       tmrpcm.play("50.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       tmrpcm.play("9.wav");
-      delay(500);
+      while(tmrpcm.isPlaying());
       break;                                       
    }
+}
+
+void AmpPowerUp() 
+{
+  digitalWrite(AmpON, HIGH); 
+  delay(500);
+}
+
+void AmpPowerDown() 
+{
+  delay(500);
+  digitalWrite(AmpON, LOW);
+  
 }
